e747a37707678845caa09997f452943c78cfe23f
gum2kevil <gum2kevil@gmail.com>
2021-10-03 16:30:39 +0700

    conver tuong phan

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print('hello')
+from PIL import Image, ImageEnhance
+# PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+img = Image.open("rick-morty.png")
+
+# Enhance constrast
+enhancer = ImageEnhance.Contrast(img)
+for i in range(1, 8):
+    factor = i / 4.0
+    new_img = enhancer.enhance(factor)
+    new_img.show()
